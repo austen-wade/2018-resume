e5189cef8b235a1cc2e1350e1a8f2a3b1411ac77
Austen Wade <austen.wade.ut@gmail.com>
2018-01-11 00:03:20 -0700

    init commit

---
+++ app/js/data.js
@@ -0,0 +1,125 @@
+var data = {
+	'introduction': `Lifelong computer geek with passion for 
+	web development &amp; web design seeking a tech career where 
+	my coding skills and attention to detail can be well utilized.`,
+	
+	'languages': [
+		{
+			'name': 'Javascript',
+			'comfort': '9'
+		},
+		{
+			'name': 'CSS',
+			'comfort': '9'
+		},
+		{
+			'name': 'PHP',
+			'comfort': '7'
+		},
+		{
+			'name': 'Python',
+			'comfort': '7'
+		},
+		{
+			'name': 'C#',
+			'comfort': '6'
+		},
+		{
+			'name': 'Java',
+			'comfort': '6'
+		}
+	],
+	'tools': [
+		{
+			'name': 'jQuery',
+			'comfort': '8'
+		},
+		{
+			'name': 'Git',
+			'comfort': '7'
+		},
+		{
+			'name': 'SASS (+SCSS)',
+			'comfort': '9'
+		},
+		{
+			'name': 'LESS',
+			'comfort': '9'
+		},
+		{
+			'name': 'Bootstrap',
+			'comfort': '10'
+		},
+		{
+			'name': 'Pug',
+			'comfort': '7'
+		},
+		{
+			'name': 'Wordpress',
+			'comfort': '5'
+		},
+		{
+			'name': 'AEM',
+			'comfort': '6'
+		},
+		{
+			'name': 'ReactJS',
+			'comfort': '4'
+		},
+		{
+			'name': 'Selenium',
+			'comfort': '8'
+		}
+	],
+	'education': [
+		{
+			'school': 'Bingham High School',
+			'date': 'Graduated 05/14'
+		},
+		{
+			'school': 'Utah State University',
+			'date': '01/16 - 05/16'
+		},
+	],
+	'experience': [
+		{
+			'workplace': 'Axis41',
+			'title': 'Quality Assurance Analyst',
+			'dates': '03/17 – Present',
+			'tasks': [
+				'Work in tight-knit Agile teams to produce client-requested deliverables in a timely manner.', 
+				'Perform smoke tests on websites during deployment.',
+				''
+			]
+		},
+		{
+			'workplace': 'The Home Depot',
+			'title': 'Service Desk Associate',
+			'dates': '10/15 – 03/17',
+			'tasks': [
+				'Assure a pleasant customer experience, front-end and floor.',
+				'Develop a collaborative environment with fellow associates.',
+				'Demonstrated HTML price matching scams to management and asset protection.',
+				'Consistently rewarded by management for excellent performance of duties.'
+			]
+		},
+		{
+			'workplace': 'CarMax Inc.',
+			'title': 'Reconditioning Associate',
+			'dates': '06/12 - 11/12',
+			'tasks': [
+				'Coordinated with team of detailers to complete responsibilities in allotted time.',
+				'Prepared and inspected vehicles before delivery to customer.'
+			]
+		}
+	],
+	'footer': {
+		links: [
+			{
+				'text': '',
+				'href': ''
+			}
+		],
+		contactInfo: []
+	}
+}
