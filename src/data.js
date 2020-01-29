7ff9b12c5e61725bc65459cc35857e647c64cf64
Austen Wade <austen.wade.ut@gmail.com>
2020-01-28 23:17:27 -0700

    updates

--- src/data.js
+++ src/data.js
@@ -1,89 +1,77 @@
-export default  {
-    introduction: `Lifelong computer geek with passion for 
+export default {
+	introduction: `Lifelong computer geek with passion for 
     web development &amp; web design seeking a tech career where 
     my coding skills and attention to detail can be well utilized.`,
 
-    languages: [
-        'Javascript',
-        'CSS',
-        'Python',
-        'PHP',
-        'Java',
-        'C#',
-        'Ruby'
-    ],
-    libraries: [
-        'Sass',
-        'Less',
-        'React',
-        'Vue.js',
-        'Gulp',
-        'Grunt',
-        'Webpack',
-        'Selenium',
-        'jQuery',
-        'GSAP',
-        'Sling',
-        'OSGi'
-    ],
-    tools: [
-        'Git',
-        'Wordpress',
-        'Adobe Experience Manager',
-        'Adobe Photoshop CC',
-        'Sketch'
-    ],
-    education: [
-        {
-            school: 'Bingham High School',
-            date: 'Graduated 05/14'
-        },
-        {
-            school: 'Utah State University',
-            date: '01/16 - 05/16'
-        },
-    ],
-    experience: [
-        {
-            workplace: 'Axis41',
-            title: 'Software Engineer',
-            dates: '03/18 – Present',
-            tasks: [
-                'Develop modular web components primarily for Content Management Systems such as Adobe Experience Manager and Wordpress.',
-                'Automate tasks as needed internally, primarily with Python.',
-                'Work in tight-knit Agile teams to produce client-requested deliverables in a timely manner.',
-                'Teach programming concepts both internally and at a high school.'
-            ]
-        },
-        {
-            workplace: 'Axis41',
-            title: 'Quality Assurance Analyst',
-            dates: '03/17 – 03/18',
-            tasks: [
-                'Provided quality assurance to a multitude of projects and websites.',
-                'Performed smoke and regression testing on websites during deployment.',
-                'Created and maintained Selenium automation framework.'
-            ]
-        },
-        {
-            workplace: 'The Home Depot',
-            title: 'Service Desk Associate',
-            dates: '10/15 – 03/17',
-            tasks: [
-                'Assured a pleasant customer experience, front-end and floor.',
-                'Developed a collaborative environment with fellow associates.',
-                'Demonstrated HTML price matching scams to management and asset protection.',
-                'Consistently rewarded by management for excellent performance of duties.'
-            ]
-        },
-        {
-            workplace: 'CarMax Inc.',
-            title: 'Reconditioning Associate',
-            dates: '06/12 - 11/12',
-            tasks: [
-                'Coordinated with team of detailers to complete responsibilities in allotted time.',
-                'Prepared and inspected vehicles before delivery to customer.'
-            ]
-        }
-    ]
-}
+	languages: ['Javascript', 'CSS', 'Python', 'PHP', 'Java', 'C#'],
+	libraries: [
+		'React',
+		'Vue.js',
+		'Sass',
+		'Less',
+		'Webpack',
+		'Selenium',
+		'GSAP',
+		'Apache Sling',
+		'OSGi'
+	],
+	tools: [
+		'Git',
+		'Adobe Experience Manager',
+		'Wordpress',
+		'Adobe Photoshop CC',
+		'Sketch'
+	],
+	education: [
+		// {
+		// 	school: 'Bingham High School',
+		// 	date: 'Graduated 05/14'
+		// },
+		// {
+		// 	school: 'Utah State University',
+		// 	date: '01/16 - 05/16'
+		// }
+	],
+	experience: [
+		{
+			workplace: 'Hoodoo Digital',
+			title: 'AEM Engineer',
+			dates: '08/19 – Present',
+			tasks: [
+				'Perform site implementations on the Adobe Experience Manager platform.',
+				'Maintain and upgrade highly regarded medical sites for client.',
+				'Collaberate directly with a client-side developer team.'
+			]
+		},
+		{
+			workplace: 'Axis41',
+			title: 'Software Engineer',
+			dates: '03/18 – 08/19',
+			tasks: [
+				'Developed modular web components primarily for Content Management Systems such as Adobe Experience Manager and Wordpress.',
+				'Automated tasks as needed internally, primarily with Python.',
+				'Worked in tight-knit Agile teams to produce client-requested deliverables in a timely manner.',
+				'Taught programming concepts both internally and at a high school.'
+			]
+		},
+		{
+			workplace: 'Axis41',
+			title: 'Quality Assurance Analyst',
+			dates: '03/17 – 03/18',
+			tasks: [
+				'Provided quality assurance to a multitude of projects and websites.',
+				'Performed smoke and regression testing on websites during deployment.',
+				'Created and maintained Selenium automation framework.'
+			]
+		},
+		{
+			workplace: 'The Home Depot',
+			title: 'Service Desk Associate',
+			dates: '10/15 – 03/17',
+			tasks: [
+				'Assured a pleasant customer experience, front-end and floor.',
+				'Developed a collaborative environment with fellow associates.'
+			]
+		}
+	]
+};
